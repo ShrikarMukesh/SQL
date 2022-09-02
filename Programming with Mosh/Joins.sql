2887d4b24cf836c651113eaa4dc04e75d80f7620
Shrikar Mukesh <mukesh.shrikar7@gmail.com>
2022-09-02 14:25:51 +0530

    Practiced SQL Queries

--- Programming with Mosh/Joins.sql
+++ Programming with Mosh/Joins.sql
@@ -3,7 +3,9 @@
 -- join customers 
 -- ON orders.customer_id = customers.customer_id;
 
-select order_id, oi.product_id , quantity, unit_price
+use sql_store;
+
+select oi.order_id, oi.product_id , quantity, oi.unit_price
 from order_items oi
 join products prd 
 ON oi.product_id = prd.product_id;
@@ -15,6 +17,18 @@ ON oi.product_id = prd.product_id;
 -- JOIN employees mag
 --    ON emp.reports_to = mag.employee_id;
 
+-- select the employees with max length and min length
+
+(select first_name , length(first_name)
+from employees
+order by length(first_name) asc , first_name asc limit 1)
+UNION ALL (
+  select first_name , length(first_name)
+  from employees
+  order by length(first_name) desc , first_name desc limit 1
+);
+
 select emp.first_name as CEO
 from employees emp
 where emp.reports_to IS NULL;
+
