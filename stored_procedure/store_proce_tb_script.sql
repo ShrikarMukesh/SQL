8969b1e816a98ff2a1dd7b191368143e742ff671
Shrikar Mukesh <mukesh.shrikar7@gmail.com>
2022-09-29 15:19:09 +0530

    stored procedure

--- stored_procedure/store_proce_tb_script.sql
+++ stored_procedure/store_proce_tb_script.sql
@@ -7,19 +7,32 @@ CREATE TABLE products (
 );
 
 ALTER TABLE products ADD CONSTRAINT products_pk PRIMARY KEY (product_code);
+
+ALTER TABLE table_name
+	ADD COLUMN id SERIAL PRIMARY KEY;
    
 select * from products;
 
 select * from sales;
 
 CREATE TABLE sales (
-   order_id integer primary key,
+   order_id SERIAL primary key ,
    order_date DATE NOT NULL DEFAULT CURRENT_DATE,
    product_code varchar(20),
    quantity_ordered integer,
    sale_price double precision,
    CONSTRAINT fk_product_code FOREIGN key (product_code) REFERENCES products(product_code)
 );
+
+CREATE TABLE books (
+  id              SERIAL PRIMARY KEY,
+  title           VARCHAR(100) NOT NULL,
+  primary_author  VARCHAR(100) NULL
+);
+
+ALTER TABLE sales
+	ADD CONSTRAINT order_id SERIAL primary key;
+
 
 INSERT INTO products(product_code,product_name,price,quantity_remaining,quantity_sold)
    VALUES('P2', 'Woodland Shoes',1479,12,2222), ('P3','boAT rockers head 255+' ,999,4,1718);	   
