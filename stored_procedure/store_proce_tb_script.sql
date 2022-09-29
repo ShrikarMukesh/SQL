7e1fdd103303e5bb603283a62dbe9a86cb6c4abb
Shrikar Mukesh <mukesh.shrikar7@gmail.com>
2022-09-29 13:04:35 +0530

    practiced stored procedure

---
+++ stored_procedure/store_proce_tb_script.sql
@@ -0,0 +1,28 @@
+CREATE TABLE products (
+   product_code varchar(20) not null,
+   product_name varchar(100) ,
+   price double precision,
+   quantity_remaining integer,
+   quantity_sold integer
+);
+
+ALTER TABLE products ADD CONSTRAINT products_pk PRIMARY KEY (product_code);
+   
+select * from products;
+
+select * from sales;
+
+CREATE TABLE sales (
+   order_id integer primary key,
+   order_date DATE NOT NULL DEFAULT CURRENT_DATE,
+   product_code varchar(20),
+   quantity_ordered integer,
+   sale_price double precision,
+   CONSTRAINT fk_product_code FOREIGN key (product_code) REFERENCES products(product_code)
+);
+
+INSERT INTO products(product_code,product_name,price,quantity_remaining,quantity_sold)
+   VALUES('P2', 'Woodland Shoes',1479,12,2222), ('P3','boAT rockers head 255+' ,999,4,1718);	   
+	   
+INSERT INTO sales(order_id, order_date, product_code, quantity_ordered,sale_price)
+values (3,'2020-02-05','P1',1,900), (4,'2019-09-11','P3',2,7673);
--- udemy/ig_clone_Queries.sql
+++ udemy/ig_clone_Queries.sql
@@ -1,3 +1,58 @@
 -- 1. find 5 oldest users
+select * from users
+order by users.created_at limit 5;
 
-select * from users;
+-- 2. What day of the week do most users register on?
+-- Most Popular registration date
+select dayname(created_at) as dayoftheweek,
+count(*) as total
+from users
+group by dayoftheweek
+order by total desc
+limit 2;
+
+-- 3. Identify Inactive Users (users with no photos)
+-- select users.id, users.username,photos.image_url
+select username, users.id
+from users
+Left Join photos 
+     ON users.id = photos.user_id
+where photos.id IS NULL;
+
+-- 4. Identify most popular photo (and user who created it)
+select 
+   users.username , photos.id, photos.image_url,
+   count(*) as total
+from photos 
+INNER JOIN likes
+ON likes.photo_id = photos.id
+inner join users
+on photos.user_id = users.id
+group by photos.id
+order by total desc limit 1;
+
+-- 5. Calculate average number of photos per user
+SELECT (SELECT Count(*) 
+        FROM   photos) / (SELECT Count(*) 
+                          FROM   users) AS avg; 
+
+-- 6. Find the five most popular hashtags
+SELECT tags.tag_name, 
+       Count(*) AS total 
+FROM   photo_tags 
+       JOIN tags 
+         ON photo_tags.tag_id = tags.id 
+GROUP  BY tags.id 
+ORDER  BY total DESC 
+LIMIT  5; 
+
+-- 7. Finding the bots - the users who have liked every single photo
+SELECT username, 
+       Count(*) AS num_likes 
+FROM   users 
+       INNER JOIN likes 
+               ON users.id = likes.user_id 
+GROUP  BY likes.user_id 
+HAVING num_likes = (SELECT Count(*) 
+                    FROM   photos); 
+   
